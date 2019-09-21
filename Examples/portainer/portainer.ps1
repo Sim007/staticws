b94c88cbb75ac10d63c90966a01dae9277256639
Sim <johannes.sim@centric.eu>
2019-09-21 11:08:36 +0200

    add portainer

---
+++ Examples/portainer/portainer.ps1
@@ -0,0 +1,2 @@
+Set-ExecutionPolicy RemoteSigned
+Invoke-Command {docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer}
